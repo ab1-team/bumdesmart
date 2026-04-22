c18f7074c6f8e45569584d231465ea0751ce03d7
arafi118 <arp46301@gmail.com>
2026-04-22 10:26:57 +0700

    fix: perbaiki modal close saat checklist role

--- resources/views/livewire/role.blade.php
+++ resources/views/livewire/role.blade.php
@@ -43,7 +43,7 @@
         </div>
     </div>
 
-    <div class="modal fade" id="roleModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div wire:ignore.self class="modal fade" id="roleModal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
